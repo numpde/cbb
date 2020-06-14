50bd43434e3c400e5af99913166e62ca67624292
RA <numpde@null.net>
2020-06-14 12:58:28 +0200

    Iteration

--- 20200612-Entropy/entropy.py
+++ 20200612-Entropy/entropy.py
@@ -3,12 +3,23 @@
 """
 Follows
     [1, Sec 2.3.3: Simple statistical genomic analysis]
-and
     [1, Sec 2.3.4: Genomic segmentation algorithm]
 
 Run 20200612-M.jannaschii/download.py to download the genome.
 
 [1] Clote & Backofen, Computational Molecular Biology: An Introduction, 2000, Wiley
+[2] Sims et al, Alignment-free genome comparison with feature
+    frequency profiles (FFP) and optimal resolutions, PNAS 2008,
+    https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2634796/pdf/zpq2677.pdf
+
+
+Note:
+    According to [2], "the R-Y alphabet has been shown to improve phylogenies
+    by removing the distorting effects of species specific
+    bias in base composition and bias in the third codon position (8, 16).
+    ...
+    Block comparisons are an absolute necessity when comparing genomes with
+    large length differences
 """
 
 import io
@@ -17,6 +28,7 @@ import numpy as np
 import pandas as pd
 
 from Bio import SeqRecord, SeqIO
+from scipy import stats
 from pathlib import Path
 from inclusive import range
 from collections import defaultdict
@@ -26,6 +38,18 @@ from more_itertools import first, windowed
 PARAM = {
     'fasta': next(Path(__file__).parent.parent.glob("**/M.jannaschii/**/*genomic.fna.gz")),
 }
+
+
+def hypergeometric(rR, gG):
+    """
+    rR = (r, R) and gG = (g, G) represent
+    a draw of r red and g green balls
+    out of an urn containing R red and G green balls.
+    """
+
+    (r, R) = rR
+    (g, G) = gG
+    return stats.hypergeom.pmf(r, R + G, R, r + g)
 
 
 def get_genome() -> SeqRecord:
@@ -93,6 +117,75 @@ def dinucleotide_entropy(sequence):
     fig.savefig("dinucleotide_entropy.png")
 
 
+def JSD(U, V):
+    """
+    U, V are frequency distributions, i.e.
+    U[a] and V[a] are nonnegative integers.
+    Returns the Jensen-Shannon divergence
+    as defined in [1, Sec 2.3.4, p.69].
+    """
+
+    W = {
+        k: U.get(k, 0) + V.get(k, 0)
+        for k in (set(U) | set(V))
+    }
+
+    nu = sum(U.values())
+    nv = sum(V.values())
+
+    js = 0
+    js += (nu / (nu + nv)) * (H(W) - H(U))
+    js += (nv / (nu + nv)) * (H(W) - H(V))
+
+    return js
+
+
+# TODO: this is too slow
+def js_statistical_significance(U: dict, V: dict):
+    """
+    Let U, V be two frequency distributions, i.e.
+    U[A] and V[A] are nonnegative integers.
+    Let W be the composition/sum U + V.
+    According to [1, Sec 2.3.4, p.69],
+    statistical significance of the segmentation W = U + V
+    is the probability that a random sequence
+    with the frequencies W, when split into two
+    subsequences with frequencies U' and V', resp.,
+    has Jensen-Shannon divergence at most JS(U', V').
+    Ignores everything but A in {'R', 'Y'}.
+    """
+
+    RY = {'R': 0, 'Y': 0}
+
+    U = {**RY, **{k: v for (k, v) in U.items() if k in RY}}
+    V = {**RY, **{k: v for (k, v) in V.items() if k in RY}}
+
+    # Total counts
+    R = U['R'] + V['R']
+    Y = U['Y'] + V['Y']
+
+    # Statistical significance
+    s = 0
+
+    (M, N) = map(sum, (U.values(), V.values()))
+
+    js0 = JSD(U, V)
+
+    # Conceivable counts in the *left* box are (r, y) with
+    #     0 <= r <= R
+    #     0 <= y <= Y
+    #     r + y = M := sum(U.values())
+    # i.e.
+    #     0 <= r <= min(R, M)
+    #     0 <= y <= min(Y, M)
+    for (r, y) in zip(range[0, M], reversed(range[0, M])):
+        if (0 <= r <= R) and (0 <= y <= Y):
+            if (JSD({'R': r, 'Y': y}, {'R': R - r, 'Y': Y - y}) <= js0):
+                s = s + hypergeometric((r, R), (y, Y))
+
+    return s
+
+
 def segmentation1(sequence):
     """
     Genomic segmentation following [1, Sec 2.3.4, p.69]
@@ -106,17 +199,21 @@ def segmentation1(sequence):
     n = 717112
     sequence = ry(sequence[0:n])
 
-    W = dict(Counter(sequence))
-
     js = defaultdict(int)
 
-    (U, V) = ({k: 0 for k in W}, dict(W))
+    (U, V) = (defaultdict(int), dict(Counter(sequence)))
+
     for (m, c) in enumerate(sequence, start=1):
         U[c] += 1
         V[c] -= 1
         if (m % 100): continue
         # Jensen-Shannon divergence
-        js[m] = (m / n) * (H(W) - H(U)) + ((n - m) / n) * (H(W) - H(V))
+        js[m] = JSD(U, V)
+
+        if (m == 300000):
+            print(F"Statistical significance at m = {m} is:", end=' ')
+            s = js_statistical_significance(U, V)
+            print(s)
 
     js = pd.Series(js).sort_index()
 
